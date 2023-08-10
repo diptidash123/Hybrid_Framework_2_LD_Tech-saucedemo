7ccf91a23623730f1fc34ddbcf59ea4a89bc51a0
Dipti Ranjan Dash <66427750+diptidash123@users.noreply.github.com>
2023-08-10 21:30:56 +0530

    Test_class.java

--- com.DiptiRanjanDash_HybridFramework_casestudy2/src/test/java/Test/Test_class.java
+++ com.DiptiRanjanDash_HybridFramework_casestudy2/src/test/java/Test/Test_class.java
@@ -1,13 +1,11 @@
 package Test;
 
 import java.io.IOException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-
 import POM.Pom_class;
 import generics.BaseTest;
 
